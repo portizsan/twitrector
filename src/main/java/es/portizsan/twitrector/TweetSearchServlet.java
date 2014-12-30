5c37744abc39527230ad231863cc378acf50f68b
Pablo Ortiz <pablo.ortiz.sanchidrian.contractor@bbva.com>
2014-12-30 15:55:04 +0100

    new SearchServlet

---
+++ src/main/java/es/portizsan/twitrector/TweetSearchServlet.java
@@ -0,0 +1,68 @@
+/**
+ * Copyright 2012 Google Inc. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package es.portizsan.twitrector;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.List;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import twitter4j.Query;
+import twitter4j.QueryResult;
+import twitter4j.Status;
+import twitter4j.Twitter;
+import twitter4j.TwitterException;
+import twitter4j.TwitterFactory;
+
+public class TweetSearchServlet extends HttpServlet {
+	private static final long serialVersionUID = -1243223937144208948L;
+
+	@Override
+	public void doGet(HttpServletRequest req, HttpServletResponse resp)
+			throws IOException {
+
+		try {
+			BufferedReader br = new BufferedReader(new InputStreamReader(
+					System.in));
+			String search = br.readLine();
+			Twitter twitter = new TwitterFactory().getInstance();
+			Query query = new Query(search);
+			QueryResult result;
+			do {
+				result = twitter.search(query);
+				List<Status> tweets = result.getTweets();
+				for (Status tweet : tweets) {
+					System.out.println("@" + tweet.getUser().getScreenName()
+							+ " - " + tweet.getText());
+				}
+			} while ((query = result.nextQuery()) != null);
+			System.exit(0);
+		} catch (TwitterException te) {
+			te.printStackTrace();
+			System.out.println("Failed to search tweets: " + te.getMessage());
+			System.exit(-1);
+		} catch (IOException e) {
+			e.printStackTrace();
+			System.out.println("IOException: " + e.getMessage());
+			System.exit(-1);
+		}
+	}
+}
